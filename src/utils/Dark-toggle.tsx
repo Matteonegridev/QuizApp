fd70803fa54cda4ddbdf2645c706e8d27c01bd75
Matteo <matteonegridev@gmail.com>
2024-10-08 13:18:16 +0200

    added transition to toggle dark mode

--- src/utils/Dark-toggle.tsx
+++ src/utils/Dark-toggle.tsx
@@ -34,7 +34,7 @@ function DarkToggle() {
 
   return (
     <button
-      className={` w-10 h-10 rounded-[50%] ${
+      className={` w-10 h-10 rounded-[50%]  ${
         isDark ? "bg-blue-400" : "bg-black"
       }  flex justify-center items-center`}
       onClick={toggleDark}
